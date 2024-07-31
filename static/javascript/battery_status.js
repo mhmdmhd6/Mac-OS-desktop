f23d93ec13fa9d3256701535ebd3f1a49b2aa05d
Ali Asghar Fathi Khah <ali.fathi.13121110@gmail.com>
2024-07-31 18:52:41 +0330

    bugfix battery_status

--- static/javascript/battery_status.js
+++ static/javascript/battery_status.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
-    function fetchData() {
-      $.ajax({
-        url: "/api/data",
-        method: "GET",
-        success: function(data) {
-          $('.battery__text').text(data.battery);
-          $('.wifi').text(data.wifi);
-          $('.bluetooth').text(data.bluetooth);
-        }
-      });
-    }
-    
-    fetchData();
-    setInterval(fetchData, 15000);
-  });
-  
+$(document).ready(function () {
+  function fetchData() {
+    $.ajax({
+      url: "/api/data",
+      method: "GET",
+      success: function (data) {
+        $('.battery__text').text(data.battery);
+        $('.wifi').text(data.wifi);
+        $('.bluetooth').text(data.bluetooth);
+        $('.charging-status').text(data.charging);
+        $('.time-left').text(data.time_left);
+      }
+    });
+  }
+
+  fetchData();
+  setInterval(fetchData, 15000);
+});
